60497792bd24447d455ce1899ed8e001ee3695ee
Parth Gamit <parthgamit483@gmail.com>
2025-06-04 13:57:04 +0530

    some fix

--- lib/chatui.dart
+++ lib/chatui.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterchatapp/chatuiCtr.dart';
+import 'package:flutterchatapp/chatUICtr.dart';
 import 'package:flutterchatapp/components/chat_bubble.dart';
 import 'package:flutterchatapp/configs/colors_constant.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterchatapp/chatui.dart';
+import 'package:flutterchatapp/chatUI.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:sizer/sizer.dart';
 
